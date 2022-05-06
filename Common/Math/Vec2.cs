d1e8d0db9e902d345b068ff354a58b54f050eb34
matthiasffm <matthias@schmidt-dot.net>
2022-05-06 20:39:34 +0200

    feat: added unit tests for A*

--- Common/Math/Vec2.cs
+++ Common/Math/Vec2.cs
@@ -1,15 +1,15 @@
-﻿namespace matthiasffm.Common.Math;
+﻿//namespace matthiasffm.Common.Math;
 
-internal class Vec2
-{
-    public int X { get; set; }
-    public int Y { get; set; }
+//public class Vec2
+//{
+//    public int X { get; set; }
+//    public int Y { get; set; }
 
-    public Vec2(int x, int y) { X = x; Y = y; }
+//    public Vec2(int x, int y) { X = x; Y = y; }
 
-    public void Add(Vec2 toAdd) { X += toAdd.X; Y += toAdd.Y; }
-    public bool In(Vec2 topLeft, Vec2 bottomRight) => X >= topLeft.X && X <= bottomRight.X &&
-                                                      Y <= topLeft.Y && Y >= bottomRight.Y;
+//    public void Add(Vec2 toAdd) { X += toAdd.X; Y += toAdd.Y; }
+//    public bool In(Vec2 topLeft, Vec2 bottomRight) => X >= topLeft.X && X <= bottomRight.X &&
+//                                                      Y <= topLeft.Y && Y >= bottomRight.Y;
 
-    public override string ToString() => $"({X}, {Y})";
-}
+//    public override string ToString() => $"({X}, {Y})";
+//}
