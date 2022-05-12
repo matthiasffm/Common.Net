9dbad242fa5a3bee8ac6ae65964590a8108af17c
matthiasffm <matthias@schmidt-dot.net>
2022-05-12 11:07:41 +0200

    feat: list selections reworked and unittested

---
+++ Common.Test/TestListSelections.cs
@@ -0,0 +1,157 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+using matthiasffm.Common.Collections;
+
+namespace matthiasffm.Common.Test;
+
+internal class TestListSelections
+{
+    [Test]
+    public void TestPartitionOnEmptyList()
+    {
+        // arrange
+
+        var emptyList = new List<int> { };
+
+        // act
+
+        var partitionIdx = emptyList.Partition();
+
+        // assert
+
+        emptyList.Should().BeEmpty();
+        partitionIdx.Should().Be(0);
+    }
+
+    /// <summary>
+    /// Prüft die in-place Partitionierung von Listen anhand deren letzten Elements n.
+    /// Partition liefert den Index x zurück und partitioniert list, so dass list[..x] <= n und n <= list[x..].
+    /// </summary>
+    [Test]
+    public void TestPartition()
+    {
+        // arrange
+
+        var listWithOneEntry = new List<int> { 2 };
+        var listToPartition1 = new List<int> { 2, 1, 7, 8, 4, 3, 10, 5 };
+        var listToPartition2 = new List<int> { 2, 7, 1, 8, 3, 4, 5, 10 };
+
+        // act
+
+        var idxForOneEntryList = listWithOneEntry.Partition();
+        var partitionIdx1 = listToPartition1.Partition();
+        var partitionIdx2 = listToPartition2.Partition();
+
+        // assert
+
+        idxForOneEntryList.Should().Be(0);
+        listWithOneEntry.Should().Equal(2);
+
+        partitionIdx1.Should().Be(4);
+        listToPartition1.GetRange(0, partitionIdx1).Should().OnlyContain(x => x <= 5);
+        listToPartition1.GetRange(partitionIdx1, 4).Should().OnlyContain(x => x >= 5);
+        listToPartition1.Should().HaveCount(8).And.BeEquivalentTo(new[] { 1, 2, 3, 4, 5, 7, 8, 10 });
+
+        partitionIdx2.Should().Be(7);
+        listToPartition2.GetRange(0, partitionIdx2).Should().OnlyContain(x => x <= 10);
+        listToPartition2.GetRange(partitionIdx2, 1).Should().OnlyContain(x => x >= 10);
+        listToPartition2.Should().HaveCount(8).And.BeEquivalentTo(new[] { 1, 2, 3, 4, 5, 7, 8, 10 });
+    }
+
+    /// <summary>
+    /// Prüft die in-place Partitionierung innerhalb einer Teilliste anhand des letzten Elements n der Teilliste.
+    /// Partition liefert den Index x zurück und partitioniert list, so dass list[p..x] <= n und n <= list[x..r].
+    /// </summary>
+    [Test]
+    public void TestPartitionSublist()
+    {
+        // arrange
+
+        var listWithOneEntry = new List<int> { 2 };
+        var listToPartition1 = new List<int> { 2, 1, 7, 8, 4, 3, 10, 5 };
+        var listToPartition2 = new List<int>(listToPartition1);
+
+        // act
+
+        var idxForOneEntryList = listWithOneEntry.Partition(0, 0);
+        var partition2To4Idx = listToPartition1.Partition(2, 4);
+        var partition1To5Idx = listToPartition2.Partition(1, 5);
+
+        // assert
+
+        idxForOneEntryList.Should().Be(0);
+        listWithOneEntry.Should().Equal(2);
+
+        partition2To4Idx.Should().Be(2); // nach Partition der Teilliste 7, 8, 4: { 2, 1, <=4, >=4, >=4, 3, 10, 5)
+        listToPartition1.GetRange(0, 2).Should().Equal(2, 1);
+        listToPartition1.GetRange(5, 3).Should().Equal(3, 10, 5);
+        listToPartition1.GetRange(2, partition2To4Idx - 1).Should().OnlyContain(x => x <= 4);
+        listToPartition1.GetRange(partition2To4Idx, 3).Should().OnlyContain(x => x >= 4);
+        listToPartition1.Should().HaveCount(8).And.BeEquivalentTo(new[] { 2, 1, 4, 7, 8, 3, 10, 5 });
+
+        partition1To5Idx.Should().Be(2); // nach Partition der Teilliste 1, 7, 8, 4, 3: { 2, <=3, <=3, >=3, >=3, =>3, 10, 5)
+        listToPartition1.GetRange(0, 1).Should().Equal(2);
+        listToPartition1.GetRange(6, 2).Should().Equal(10, 5);
+        listToPartition2.GetRange(1, partition1To5Idx).Should().OnlyContain(x => x <= 3);
+        listToPartition2.GetRange(partition1To5Idx, 4).Should().OnlyContain(x => x >= 3);
+        listToPartition2.Should().HaveCount(8).And.BeEquivalentTo(new[] { 2, 1, 4, 7, 8, 3, 10, 5 });
+    }
+
+    [Test]
+    public void TestMedian()
+    {
+        // arrange
+
+        var emptyList        = new List<int> { };
+        var listWithOneEntry = new List<int> { 2 };
+        var listEven         = new List<int> { 2, 1, 7, 4, 10, 11, 8, 5 };
+        var listOdd          = new List<int> { 2, 1, 7, 8, 4, 3, 5 };
+
+        // act
+
+        var medianForEmptyList          = () => emptyList.Median();
+        var medianForListWithOneEntry   = listWithOneEntry.Median();
+        var medianForEvenList           = listEven.Median();
+        var medianForOddList            = listOdd.Median();
+
+        // assert
+
+        medianForEmptyList.Should().Throw<ArgumentOutOfRangeException>("list");
+        medianForListWithOneEntry.Should().Be(2);
+        medianForEvenList.Should().Be(5);
+        medianForOddList.Should().Be(4);
+    }
+
+    [Test]
+    public void TestNthSmallest()
+    {
+        // arrange
+
+        var emptyList        = new List<int> { };
+        var listWithOneEntry = new List<int> { 2 };
+        var list             = new List<int> { 2, 1, 7, 4, 10, 11, 8, 5 };
+
+        // act
+
+        var nthSmallestForEmptyList         = () => emptyList.NthSmallest(0);
+        var nthSmallestForListWithOneEntry  = listWithOneEntry.NthSmallest(1);
+        var nthSmallest0                    = () => list.NthSmallest(0);
+        var nthSmallest9                    = () => list.NthSmallest(9);
+        var nthSmallest1                    = list.NthSmallest(1);
+        var nthSmallest2                    = list.NthSmallest(2);
+        var nthSmallest4                    = list.NthSmallest(4);
+        var nthSmallest8                    = list.NthSmallest(8);
+
+        // assert
+
+        nthSmallestForEmptyList.Should().Throw<ArgumentOutOfRangeException>("list");
+        nthSmallestForListWithOneEntry.Should().Be(2);
+        nthSmallest0.Should().Throw<ArgumentOutOfRangeException>("nth");
+        nthSmallest9.Should().Throw<ArgumentOutOfRangeException>("nth");
+        nthSmallest1.Should().Be(1);
+        nthSmallest2.Should().Be(2);
+        nthSmallest4.Should().Be(5);
+        nthSmallest8.Should().Be(11);
+    }
+}
--- Common/Collections/ListSelections.cs
+++ Common/Collections/ListSelections.cs
@@ -1,18 +1,21 @@
-namespace matthiasffm.Common.Algorithms;
+namespace matthiasffm.Common.Collections;
 
-public static class Select
+using matthiasffm.Common.Algorithms;
+
+public static class ListSelections
 {
     /// <summary>
     /// Partitioniert eine List in place und findet einen Partitionsindex x, so dass in list[...x-1] alle Elemente kleiner gleich list[x] und
     /// alle Elemente in list[x+1...] größer gleich list[x] sind.
     /// </summary>
     /// <param name="list">die zu partitionierende Liste</param>
-    /// <result>Partitionsindex x</result>
+    /// <returns>Partitionsindex x</returns>
+    /// <remarks>Laufzeit O(n)</remarks>
     public static int Partition<T>(this IList<T> list) where T : IComparable
     {
         ArgumentNullException.ThrowIfNull(list);
 
-        return Partition(list, 0, list.Count - 1);
+        return list.Count > 0 ? list.Partition(0, list.Count - 1) : 0;
     }
 
     /// <summary>
@@ -22,7 +25,8 @@ public static class Select
     /// <param name="list">die zu partitionierende Liste</param>
     /// <param name="p">Index, ab dem die Liste partitioniert werden soll</param>
     /// <param name="r">Index, bis zu dem die Liste partitioniert werden soll</param>
-    /// <result>Partitionsindex x</result>
+    /// <returns>Partitionsindex x</returns>
+    /// <remarks>Laufzeit O(n)</remarks>
     public static int Partition<T>(this IList<T> list, int p, int r) where T : IComparable
     {
         ArgumentNullException.ThrowIfNull(list);
@@ -45,19 +49,34 @@ public static class Select
     }
 
     /// <summary>Ermittelt den Median in einer unsortierten Liste.</summary>
-    public static T Median<T>(this IList<T> list)  where T : IComparable
+    /// <remarks>Laufzeit O(n)</remarks>
+    public static T Median<T>(this IList<T> list) where T : IComparable
     {
         ArgumentNullException.ThrowIfNull(list);
+
+        if(list.Count == 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(list), "List has no elements");
+        }
+
         return list.NthSmallest((list.Count & 1) == 1 ? (list.Count + 1) / 2 : list.Count / 2);
         // return list.NthSmallest((list.Count & 1) == 1 ? list.Count / 2 : list.Count / 2 - 1);
     }
 
     /// <summary>Ermittelt das n-kleinste Element einer Liste</summary>
+    /// <param name="list">unsortierte Liste, in der das n-kleinste Element gesucht wird</param>
+    /// <param name="nth">Index des gesuchten n-kleinste Element (1...list.Count)</param>
+    /// <returns>n-kleinstes Element in list</returns>
+    /// <remarks>Laufzeit O(n)</remarks>
     public static T NthSmallest<T>(this IList<T> list, int nth) where T : IComparable
     {
         if(list == null || list.Count == 0)
         {
-            throw new ArgumentException("List has no elements");
+            throw new ArgumentOutOfRangeException(nameof(list), "List has no elements");
+        }
+        if(nth <= 0 || nth > list.Count) 
+        {
+            throw new ArgumentOutOfRangeException(nameof(nth));
         }
 
         return list.SelectNth(0, list.Count - 1, nth);
