66e7049eb145b4ce4ba56345ed48bbc01c9c8f3c
matthiasffm <matthias@schmidt-dot.net>
2024-01-15 11:37:40 +0100

    feat: added some math constants for π/2

---
+++ Common/Math/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace matthiasffm.Common.Math
+{
+    /// <summary>
+    /// Common math constants
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// PI/2 as a double
+        /// </summary>
+        public const double HalfPI = System.Math.PI / 2.0;
+
+        /// <summary>
+        /// PI/2 as a float
+        /// </summary>
+        public const float HalfPIf = (float)HalfPI;
+    }
+}
