eec07a53f3d7255d815b7955a640b61f11b49d51
matthiasffm <matthias@schmidt-dot.net>
2024-01-16 18:50:31 +0100

    opt: levenshtein in O(n²) speed and O(n) space instead of O(n³) speed
    and small bugfix for OSA algorithm

--- Common/Text/StringExtensions.cs
+++ Common/Text/StringExtensions.cs
@@ -4,12 +4,12 @@ using System.Text;
 namespace matthiasffm.Common.Text;
 
 /// <summary>
-/// Stellt Erweiterungsmethoden für Zeichenketten bereit.
+/// Provides extension methods for analyzing and manipulating strings.
 /// </summary>
 public static class StringExtensions
 {
     /// <summary>
-    /// Entfernt Akzente aus einzelnen Zeichen. Aus ä wird a, aus ć wird c, aus Š wird S und aus ž wird z.
+    /// Removes accents from every character in a string, for instance ä becomes a, ć becomes c, Š becomes S or ž becomes z.
     /// </summary>
     public static string RemoveAccents(this string s)
     {
@@ -21,7 +21,7 @@ public static class StringExtensions
     }
 
     /// <summary>
-    /// Entfernt Punkte und weitere überflüssige Zeichen aus Namen.
+    /// Removes punctuation characters from a string.
     /// </summary>
     public static string RemovePunctuation(this string s)
     {
@@ -29,13 +29,18 @@ public static class StringExtensions
 
         return s.Replace("'", string.Empty, StringComparison.OrdinalIgnoreCase)
                 .Replace(",", string.Empty, StringComparison.OrdinalIgnoreCase)
+                .Replace("!", string.Empty, StringComparison.OrdinalIgnoreCase)
+                .Replace("?", string.Empty, StringComparison.OrdinalIgnoreCase)
                 .Replace(".", string.Empty, StringComparison.OrdinalIgnoreCase);
     }
 
     /// <summary>
-    /// Parst eine Zeichenkette in eine Zahl. Wenn die Zeichenkette ungültig ist oder
-    /// keine Umwandlung erlaubt, liefert die Methode den <paramref name="defaultValue"/> zurück.
+    /// Tries to convert the number in a string to an integer.
+    /// If no conversion is possible the method returns the <paramref name="defaultValue"/>.
     /// </summary>
+    /// <remarks>
+    /// No exception is thrown.
+    /// </remarks>
     public static int SafeParseInt(this string s, int defaultValue = 0)
     {
         int val = defaultValue;
@@ -48,47 +53,68 @@ public static class StringExtensions
         return val;
     }
 
-    // TODO: optimalere Implementierung bereitstellen, die nicht alle Varianten jedesmal komplett
-    //       rekursiv in O(3^length) durchrechnet, sondern per dynamischer Programmierung einmal berechnete Distancen
-    //       abspeichert und wiederverwendet.
-
     /// <summary>
-    /// Ermittelt die Levenshtein-Distanz zwischen 2 Zeichenketten, also die kleinste Anzahl an gelöschten, eingefügten oder vertauschten
-    /// einzelnen Buchstaben, damit die zwei Zeichenketten übereinstimmen.
+    /// Computes the Levenshtein distance between two strings as the smallest number of single character edits (deletions, insertions and swaps)
+    /// to change one string into the other.
     /// </summary>
     /// <remarks>
-    /// siehe https://en.wikipedia.org/wiki/Levenshtein_distance
+    /// see https://en.wikipedia.org/wiki/Levenshtein_distance
+    ///
+    /// Runtime complexity is O(n²) and memory required is O(n).
     /// </remarks>
     public static int Levenshtein(this string left, string right)
     {
-        return LevenshteinSpan(left, right);
-    }
+        ArgumentNullException.ThrowIfNull(left);
+        ArgumentNullException.ThrowIfNull(right);
 
-    private static int LevenshteinSpan(ReadOnlySpan<char> left, ReadOnlySpan<char> right)
-    {
-        if(left.Length == 0 || right.Length == 0)
+        // essentially compute a matrix MxN of character edits where every position is a min of the 3 edits
+        // - delete char from left char
+        // - delete char from right string
+        // - swap chars
+        // the end result for minimum numbers of character edits is then the bottom right cell in the matrix
+        //
+        // here is a matrix for the Levenshtein distance between lawn and flaw:
+        //     f l a w
+        //   0 1 2 3 4
+        // l 1 1 1 2 3
+        // a 2 2 2 1 2
+        // w 3 3 3 2 1
+        // n 4 4 4 3 2 <=
+
+        // to reduce the memory footprint this method only keeps the last two rows of the matrix in memory
+        // this is possible because to compute a value in the matrix one needs only the values directly to
+        // the left, the top and the top left of this value.
+
+        var d1 = new int[left.Length + 1];
+        var d2 = new int[left.Length + 1];
+
+        for(var i = 0; i <= left.Length; i++)
         {
-            return System.Math.Max(left.Length, right.Length);
+            d1[i] = i;
         }
 
-        if(left[0] == right[0])
+        for(var j = 1; j <= right.Length; j++)
         {
-            return LevenshteinSpan(left[1..], right[1..]);
+            d2[0] = j;
+
+            for(var i = 1; i <= left.Length; i++)
+            {
+                var swapCost = (left[i - 1] == right[j - 1]) ? 0 : 1;
+
+                d2[i] = Min(d2[i - 1] + 1,         // remove a char from left string
+                            d1[i] + 1,             // remove a char from right string
+                            d1[i - 1] + swapCost); // swap chars from left and right
+            }
+
+            (d1, d2) = (d2, d1);
         }
-        else
-        {
-            return 1 + Min(LevenshteinSpan(left[1..], right),          // Entfernung für erster char in left gelöscht
-                           LevenshteinSpan(left, right[1..]),          // Entfernung für erster char in right gelöscht
-                           LevenshteinSpan(left[1..], right[1..]));    // Entfernung für erster char in left und right vertauscht
-        }
+
+        return d1[left.Length];
     }
 
     /// <summary>
-    /// Ermittelt die Distanz zwischen 2 Zeichenketten per optimal-string-alignment-Algorithms, der .
+    /// Computes the distance between two strings by using the Optimal String Alignment algorithm (OSA).
     /// </summary>
-    /// <remarks>
-    /// siehe https://en.wikipedia.org/wiki/Levenshtein_distance
-    /// </remarks>
     public static int OptimalStringAlignment(this string left, string right)
     {
         ArgumentNullException.ThrowIfNull(left);
@@ -97,6 +123,8 @@ public static class StringExtensions
         return OsaDistance(left, right, left.Length, right.Length);
     }
 
+    // TODO: replace O(n³) recursive calls with a linear algorithm like for Levenshtein distance
+    //       see https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance
     private static int OsaDistance(string left, string right, int i, int j)
     {
         if(i == 0 && j == 0)
@@ -104,7 +132,15 @@ public static class StringExtensions
             return 0;
         }
 
-        int min = i > 0 ? OsaDistance(left, right, i - 1, j) + 1 : OsaDistance(left, right, i, j - 1) + 1;
+        int min = int.MaxValue;
+        if(i > 0)
+        {
+            min = System.Math.Min(min, OsaDistance(left, right, i - 1, j) + 1);
+        }
+        if(j > 0)
+        {
+            min = System.Math.Min(min, OsaDistance(left, right, i, j - 1) + 1);
+        }
 
         if(i > 0 && j > 0)
         {
